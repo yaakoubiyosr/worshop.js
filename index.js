ec16f4251ae1bd27ed93d6d5aeacf7323dd6e23a
yosr <yassourayaakoubi@gmail.com>
2024-09-02 20:40:45 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+
+
+  ///////////////////////////////////////////////      Fibonacci         /////////////////////////////////////////////////////
+// var arr=[1,2]
+// var som=0
+// function suite(arr1,arr2,len){
+//   var arr=[arr1,arr2]
+//      for(i=0 ;i<len;i++){
+
+//       som = arr1+arr2
+//       arr.push(som) 
+//       arr1=arr2
+//       arr2=som
+//     }
+//     return arr
+//      }
+// console.log(suite(1,2,10))
+
+
+
+
+//////////////////////////////////////         Reverse a String       ///////////////////////////////////////////////////////////////////////
+// function reverse(res) {
+//     var reversed = ""; 
+//     for (var i = res.length - 1; i >= 0; i--) { 
+//       reversed += res[i]; 
+//     return reversed; 
+//   }
+// }
+//   console.log(reverse('hello')); 
+
+
+
+  ///////////////////////////////////////////////    number of characters  ////////////////////////////////////////////////////////////////////
+//   function characters(num){
+   
+//     return num.length 
+//   }
+// console.log(characters("hi"))
+
+
+
+////////////////////////////////////////////////// max et min /////////////////////////////////////////////////////////////////////////////////
+
+function maxmin(arr){
+  min=0
+  max=0
+  res = 0
+  for (i=0;i<arr.length;i++){
+    for(j=0 ; j<arr[i].length ; j++){
+        
+
+
+    }
+  }
+  return min , max
+}
+console.log( maxmin([1,5,8,7,1,3,4]))
+
+
+
